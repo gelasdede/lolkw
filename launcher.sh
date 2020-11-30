00d79aadaffb00951c86b935f6976e3b8f98902b
miningprofit <60786268+miningprofit@users.noreply.github.com>
2020-11-30 20:40:12 +0700

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -2,7 +2,8 @@
 
 POOL=asia1-etc.ethermine.org:4444
 WALLET=0xf339db86fafc99f85619a0d55e2d1e47ef9a840a
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 
 cd "$(dirname "$0")"
 
-chmod +x ./lolMiner && ./lolMiner --algo ETCHASH --pool $POOL --user $WALLET.$ip $@
+chmod +x ./lolMiner && ./lolMiner --algo ETCHASH --pool $POOL --user $WALLET.$WORKER $@
