346b8f54333b8805f61a9ed65adfc28002253fc5
miningprofit <60786268+miningprofit@users.noreply.github.com>
2020-11-30 20:33:31 +0700

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,24 +1,17 @@
+
 #!/bin/bash
-i=0;
-algo="";
-configFile="config_cmdline.ini"
-> $configFile;
-while [[ "$#" -gt 0 ]]; do
-  if echo "$1" | grep -q "algo"; then
-    algo=$1;
-  else
-    if (($((i % 2)) == 0)); then
-      a=$1;
-      echo -n ${a:1}= >> $configFile;
-    else
-      if [ "$algo" == "" ]; then
-        echo $1 >> $configFile;
-      else
-        echo [$1] >> $configFile;
-      fi
-      algo="";
-    fi
-  fi
-  i=$((i+1));
-shift; done
-chmod +x ./lolMiner && ./lolMiner $configFile
+
+#################################
+## Begin of user-editable part ##
+#################################
+
+POOL=asia1-etc.ethermine.org:4444
+WALLET=0xf339db86fafc99f85619a0d55e2d1e47ef9a840a.lolMinerWorker
+
+#################################
+##  End of user-editable part  ##
+#################################
+
+cd "$(dirname "$0")"
+
+chmod +x ./lolMiner && ./lolMiner --algo ETCHASH --pool $POOL --user $WALLET $@
