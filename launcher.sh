c57d1b006a0025bcef179eb8e7b4812a80bd652e
miningprofit <60786268+miningprofit@users.noreply.github.com>
2020-11-30 20:34:08 +0700

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,16 +1,7 @@
-
 #!/bin/bash
-
-#################################
-## Begin of user-editable part ##
-#################################
 
 POOL=asia1-etc.ethermine.org:4444
 WALLET=0xf339db86fafc99f85619a0d55e2d1e47ef9a840a.lolMinerWorker
-
-#################################
-##  End of user-editable part  ##
-#################################
 
 cd "$(dirname "$0")"
 
